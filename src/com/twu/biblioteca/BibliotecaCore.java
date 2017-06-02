7bfe3906c5bfd2c1a24acbe227846ddfa0754808
qspeng <joker_e@163.com>
2017-06-02 09:21:30 +0800

    implement the case-2

--- src/com/twu/biblioteca/BibliotecaCore.java
+++ src/com/twu/biblioteca/BibliotecaCore.java
@@ -2,17 +2,29 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.model.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Administrator on 2017/6/2.
- */
+
 public class BibliotecaCore {
+
+    private List<Book> bookList;
+
+    public BibliotecaCore() {
+        bookList = new ArrayList<Book>();
+        bookList.add(new Book());
+        bookList.add(new Book());
+        bookList.add(new Book());
+        bookList.add(new Book());
+        bookList.add(new Book());
+        bookList.add(new Book());
+    }
+
     public String getWelcomeMessage() {
         return "Welcome to the Bibliteca!";
     }
 
     public List<Book> getListBooks() {
-        return null;
+        return bookList;
     }
 }
--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -1,7 +1,10 @@
 package com.twu.biblioteca.model;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/6/2.
  */
 public class Book {
 }
+
