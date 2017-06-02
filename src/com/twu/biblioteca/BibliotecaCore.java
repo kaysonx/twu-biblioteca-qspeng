68f0748692a2f13e13f01d0b4da39a9bab27c33e
qspeng <joker_e@163.com>
2017-06-02 10:36:51 +0800

    implement the case-8

--- src/com/twu/biblioteca/BibliotecaCore.java
+++ src/com/twu/biblioteca/BibliotecaCore.java
@@ -49,7 +49,11 @@ public class BibliotecaCore {
     }
 
     public String checkout(final String bookName) {
-        bookList = bookList.stream().filter(book -> !book.getName().equals(bookName)).collect(Collectors.<Book>toList());
+        boolean searchResult = bookList.stream().anyMatch(book -> book.getName().equals(bookName));
+        if (searchResult) {
+            bookList = bookList.stream().filter(book -> !book.getName().equals(bookName)).collect(Collectors.<Book>toList());
+            return "Thank you! Enjoy the book";
+        }
         return null;
     }
 }
