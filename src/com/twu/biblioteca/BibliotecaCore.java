c5c505946d5e15d8fa2bc5a5ff54f46a4102788e
qspeng <joker_e@163.com>
2017-06-02 10:02:57 +0800

    implement the case-5

--- src/com/twu/biblioteca/BibliotecaCore.java
+++ src/com/twu/biblioteca/BibliotecaCore.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 import com.twu.biblioteca.model.Book;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -13,10 +14,10 @@ public class BibliotecaCore {
 
     public BibliotecaCore() {
         bookList = new ArrayList<Book>();
-        bookList.add(new Book("jlan,jiang","2014.08","ASP.NET MVC"));
-        bookList.add(new Book("Keith,J","2011.04","JavaScript DOM"));
-        bookList.add(new Book("dbo,lin","2013.12"," HTML5+CSS3"));
-        bookList.add(new Book("qsepng","2017.06","testBook"));
+        bookList.add(new Book("jlan,jiang", "2014.08", "ASP.NET MVC"));
+        bookList.add(new Book("Keith,J", "2011.04", "JavaScript DOM"));
+        bookList.add(new Book("dbo,lin", "2013.12", " HTML5+CSS3"));
+        bookList.add(new Book("qsepng", "2017.06", "testBook"));
 
         mainMenu = new ArrayList<String>();
         mainMenu.add("List Books");
@@ -35,6 +36,11 @@ public class BibliotecaCore {
     }
 
     public List<String> order(int order) {
-        return null;
+        List<String> orderMessage = new ArrayList<String>();
+        if (mainMenu.size() < order) {
+            orderMessage.add("Select a valid option!");
+            return orderMessage;
+        }
+        return orderMessage;
     }
 }
